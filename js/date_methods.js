872ffe6022f285cbeed68a1b4c01a09aeb085e27
Aniketch45 <aniketchafekar2004@gmail.com>
2025-09-04 18:11:30 +0530

    object clock

---
+++ js/date_methods.js
@@ -0,0 +1,11 @@
+var now=new Date();
+
+document.write(now +"<br>");
+document.write(now.toDateString()+"<br>");
+document.write(now.getDate()+ "<br>");
+document.write(now.getMonth()+1+"<br>");
+document.write(now.getFullYear()+"<br>");
+document.write(now.getHours()+":"+now.getMinutes()+":"+ now.getSeconds()+"<br>");
+document.write(now.getMilliseconds()+"<br>");
+document.write(now.toLocaleTimeString()+"<br>");
+document.write(now.toLocaleDateString()+"<br>");
---
+++ js/interpolation.js
@@ -0,0 +1,22 @@
+const Uid= "U01223";
+var FirstName= "Aniket";
+var LastName= "Chafekar";
+var Email= "aniketch6855@gmail.com";
+var Password="12345";
+var ConfirmPassword= "12345";
+var Country= "India";
+var City="Pune";
+var State="Maharashtra";
+var courseCount= 0;
+var isLoggedFromGoogle= false;
+console.log(`
+    With Unique  ID: ${Uid}
+    User is: ${FirstName}
+    Surname is: ${LastName}
+    User Email: ${Email}
+    Password is: ${Password}
+    Country is: ${Country}
+    State is: ${State}
+    City is: ${City}
+    Logged From Google: ${isLoggedFromGoogle}
+     `);
