9f2f955f6553a59d1b68534d9db9a7c85ce26541
Aniketch45 <aniketchafekar2004@gmail.com>
2025-09-05 23:14:50 +0530

    form validatins learned

---
+++ js/form_validation.js
@@ -0,0 +1,11 @@
+get form values
+const name= document.getElementById("name").value.trim();
+const email = document.getElementById("email").value.trim();
+const mobile = document.getElementById("mnumber").value.trim();
+const password = document.getElementById("password").value.trim();
+const confirmPassword = document.getElementById("confirmPassword").value.trim();
+const message = document.getElementById("message").value.trim();
+const agree = document.getElementById("agree").value.trim();
+const gender = document.querySelector('input[name="gender"]:checked');
+
+error spans
