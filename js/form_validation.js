40ad16293792d581b99b60041409726a4350664f
Aniketch45 <aniketchafekar2004@gmail.com>
2025-09-06 11:57:07 +0530

    form validation

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,4 +1,4 @@
-get form values
+// get form values
 const name= document.getElementById("name").value.trim();
 const email = document.getElementById("email").value.trim();
 const mobile = document.getElementById("mnumber").value.trim();
@@ -8,4 +8,4 @@ const message = document.getElementById("message").value.trim();
 const agree = document.getElementById("agree").value.trim();
 const gender = document.querySelector('input[name="gender"]:checked');
 
-error spans
+// error spans
